65ae9b5fba08c1fe3c4d741cad9d773a214586a6
Infant Frontender <newbornfrontender@gmail.com>
2018-09-04 14:34:53 +0300

    Исправил кнопки (были перепутаны названия)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { h } from 'hyperapp';
 export default (state, actions) => (
   <main>
     <h1>{ state.count }</h1>
-    <button onclick={ actions.down } disabled={ state.count <= 0 }>Add</button>
-    <button onclick={ actions.up }>Rm</button>
+    <button onclick={ actions.down } disabled={ state.count <= 0 }>Remove</button>
+    <button onclick={ actions.up }>Add</button>
   </main>
 );
