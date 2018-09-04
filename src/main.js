9e850df59a7e0e7c22fa856cb889722e72d19c3e
Infant Frontender <newbornfrontender@gmail.com>
2018-09-04 14:33:18 +0300

    Добавил простой пример с счетчиком

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import { app } from 'hyperapp';
+import view from './App';
+
+const state = {
+  count: 0,
+};
+
+const actions = {
+  down: () => state => ({ count: state.count - 1 }),
+  up: () => state => ({ count: state.count + 1 }),
+};
+
+app(state, actions, view, document.body);
