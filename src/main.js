dc6919d72ae5f00131ef7aaa7893c276ccb785fd
Infant Frontender <newbornfrontender@gmail.com>
2018-09-04 21:46:12 +0300

    Вынес лишнее в компоненты

--- src/main.js
+++ src/main.js
@@ -1,40 +1,18 @@
 import { h, app } from "hyperapp";
-import { Link, Route, location } from "@hyperapp/router";
+import { location } from "@hyperapp/router";
 
-import Home from './views/Home.js';
-import About from './views/About.js';
-import Topics from './views/Topics/Index.js';
+import App from './App';
 
 const state = {
-  location: location.state
-}
+  location: location.state,
+};
 
 const actions = {
-  location: location.actions
-}
+  location: location.actions,
+};
 
-const view = state => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
+const view = state => <App />;
 
-    <hr />
+const main = app(state, actions, view, document.body);
 
-    <Route path="/" render={Home} />
-    <Route path="/about" render={About} />
-    <Route parent path="/topics" render={Topics} />
-  </div>
-)
-
-const main = app(state, actions, view, document.body)
-
-const unsubscribe = location.subscribe(main.location)
+const unsubscribe = location.subscribe(main.location);
