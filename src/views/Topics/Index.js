df62926109ed01f6fe5480260678f7e3ceed3f0a
Infant Frontender <newbornfrontender@gmail.com>
2018-09-06 14:51:44 +0300

    Тестировал ресайз окна

--- src/views/Topics/Index.js
+++ src/views/Topics/Index.js
@@ -5,6 +5,16 @@ import Components from './Components';
 import SingleStateTree from './SingleStateTree';
 import Routing from './Routing';
 
+var mql = window.matchMedia('(max-width: 600px)');
+
+function screenTest(e) {
+  if (e.matches) {
+    return true
+  } else {
+    return false
+  }
+}
+
 export default ({ match }) => (
   <div key="topics">
     <h1>Topics</h1>
@@ -22,9 +32,29 @@ export default ({ match }) => (
 
     {match.isExact && <h3>Please select a topic.</h3>}
 
-    <Route parent path={`${match.path}/components`} render={Components} />
-    <Route parent path={`${match.path}/single-state-tree`} render={SingleStateTree} />
-    <Route parent path={`${match.path}/routing`} render={Routing} />
+    {window.matchMedia('(max-width: 768px)').addListener(e => {
+      return console.log(e.matches);
+    })}
+
+    {window.matchMedia('(max-width: 768px)').addListener(e => {
+      return e.matches ? <p>true</p> : <p>false</p>;
+    })}
+
+    <Route
+      parent
+      path={`${match.path}/components`}
+      render={Components}
+    />
+    <Route
+      parent
+      path={`${match.path}/single-state-tree`}
+      render={SingleStateTree}
+    />
+    <Route
+      parent
+      path={`${match.path}/routing`}
+      render={Routing}
+    />
   </div>
 );
 
